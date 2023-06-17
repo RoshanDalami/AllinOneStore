0010967a626eb3cf349e73c805cfe837d6adc35a
rsunar <tech.channel.002@gmail.com>
2023-06-17 19:50:03 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/login",
-        element: <Login/>,s
+        element: <Login/>,
       },
       {
         path: "/orderplaced",
